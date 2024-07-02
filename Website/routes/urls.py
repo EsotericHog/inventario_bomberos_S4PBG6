47f61854bab2302052ebd656dee05a9fee1678e8
Pelao con Beatle <juan.castillo18@outlook.com>
2024-07-02 00:42:00 -0400

    Nueva página de participantes, logos de institución, y modificaciones a header, nav y footer. Modificación de estilos css

--- Website/routes/urls.py
+++ Website/routes/urls.py
@@ -3,9 +3,13 @@ from django.urls import path, include
 #Módulos de vistas
 from Website.views.home import *
 from Website.views.access import *
+from Website.views.about import about
 
 urlpatterns = [
     #RUTA PRINCIPAL
     #Inicio
     path('', home, name="website-ruta_home"),
+
+    #Información del proyecto
+    path('about', about, name="website-ruta_about"),
 ]
